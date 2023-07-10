aef68ef408e6fcbb1981a2de74f3ce676838b511
Oluwasegun02 <adebayoogunniyi8@gmail.com>
2023-07-10 23:01:20 +0100

    Python - Async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 
-
-import asyncio
-from random import uniform
-
-
 ''' Write an asynchronous coroutine that takes in an integer
     argument (max_delay, with a default value of 10) named
     wait_random that waits for a random delay between 0 and
     max_delay (included and float value) seconds and eventually returns it.
 '''
-
+import asyncio
+from random import uniform
 
 async def wait_random(max_delay: int = 10) -> float:
     '''Generates a random number and returns it after n delay'''
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 
-
-from typing import List
-
-
 ''' Take the code from wait_n and alter it into a new function task_wait_n.
     The code is nearly identical to wait_n except task_wait_random is being
     called.
 '''
-
+from typing import List
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''Runs an async function for n times and adds the results into a list'''
